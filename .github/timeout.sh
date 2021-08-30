9b14c46e570035965680ab32f6edee4fa4efd2d4
tanvir-ux <tanviralamshuvo@gmail.com>
2021-08-30 17:12:48 +0600

    Bug fix

--- .github/timeout.sh
+++ .github/timeout.sh
@@ -6,9 +6,11 @@ node scrape.js
 echo "crawling done..."
 sleep 1s
 echo "pushing to github..."
+git config user.email "tanviralamshuvo@gmail.com"
+git config user.name "tanvir-ux"
 git add .
 git commit -m "$now"
-git push 
+git push origin HEAD:main
 
 
 
