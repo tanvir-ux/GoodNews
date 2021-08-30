805d4391e3a41cf2a68b1c86130810487c322fa6
tanvir-ux <tanviralamshuvo@gmail.com>
2021-08-30 17:24:18 +0600

    automated v2

--- .github/timeout.sh
+++ .github/timeout.sh
@@ -6,11 +6,6 @@ node scrape.js
 echo "crawling done..."
 sleep 1s
 echo "pushing to github..."
-git config user.email "tanviralamshuvo@gmail.com"
-git config user.name "tanvir-ux"
-git add .
-git commit -m "$now"
-git push origin HEAD:main
 
 
 
